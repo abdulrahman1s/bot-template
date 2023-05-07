196e215b890b38b25cb25df91cea4545a7d6e26c
Abdulrahman Salah <61483023+abdulrahman1s@users.noreply.github.com>
2023-05-07 22:11:37 +0200

    update

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,4 +5,8 @@
 export const ready = async (client) => {
 	console.log('Connected to Discord API')
 	console.log(client.user.tag)
+
+	const commands = [...client.commands.values()].map((it) => it.data.setDMPermission(false))
+
+	await client.application.commands.set(commands)
 }
